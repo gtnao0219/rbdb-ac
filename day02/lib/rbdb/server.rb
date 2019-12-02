e0e865d2846087c1e6832ce8fda6d756801d72d8
naotaka nakane <n.nakane0219@gmail.com>
2019-12-02 22:16:30 +0900

    day02

---
+++ day02/lib/rbdb/server.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'webrick'
+require 'rbdb/query/lexer'
+
+module Rbdb
+  class Server
+    def initialize(host: 'localhost', port: 40219)
+      @host = host
+      @port = port
+    end
+
+    def start
+      server.mount_proc '/exec' do |req, res|
+        query = req.query['query']
+        tokens = Rbdb::Query::Lexer.new(query).scan
+        # TODO:
+        tokens.each do |token|
+          res.body += token.inspect
+          res.body += "\n"
+        end
+      end
+      trap('INT') do |_|
+        terminate
+      end
+      server.start
+      terminate
+    rescue Interrupt
+      terminate
+    end
+
+    private
+
+    def terminate
+      server.shutdown
+      puts "\ngood bye!"
+    end
+
+    def server
+      @server ||= WEBrick::HTTPServer.new({
+        DocumentRoot: '/',
+        BindAddress: @host,
+        Port: @port,
+      })
+    end
+  end
+end
